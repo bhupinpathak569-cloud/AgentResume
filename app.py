39d11aafc76b3621ca6debbd0f8a34c815b2c91d
bhupinpathak569-cloud <bhupinpathak569@gmail.com>
2026-07-30 10:42:37 +0530

    Refactor app.py for improved readability and structure

--- app.py
+++ app.py
@@ -25,9 +25,9 @@ st.sidebar.title("Fill Important Details")
 st.sidebar.image("bg.png")
 
 
-google_api_key = "st.sidebar.text_input("Gemini-API",type = "password")
-groq_api_key = "st.sidebar.text_input("Groq-API",type = "password")
-tavily_api_key = "st.sidebar.text_input("Tavily-API,type = "password")
+google_api_key = "st.sidebar.text_input("Gemini-api",type = "password")
+groq_api_key = "st.sidebar.text_input("Groq-api",type = "password")
+tavily_api_key = "st.sidebar.text_input("Tavily-api,type = "password")
 print("done")
 model = ChatGoogleGenerativeAI(
     model = 'gemini-3.5-flash-lite',
